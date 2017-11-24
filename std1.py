f6e88508e7f44fb3a6b5a7d538d8e34f3259690b
lsh <sseung0703@inha.edu>
2017-11-24 22:50:48 +0900

    remove all comments

--- std1.py
+++ std1.py
@@ -13,8 +13,6 @@ def deepnn(x):
     with tf.name_scope('reshape'):                        
         x_image = tf.reshape(x, [-1, 28, 28, 1])           
                                                  
-    
-    # First convolutional layer - maps one grayscale image to 32 feature maps.
     with tf.name_scope('block0'):
         with tf.name_scope('conv0'):
             W_conv = weight_variable([3, 3, 1, 16])
@@ -50,8 +48,6 @@ def deepnn(x):
 
         h_pool = max_pool_2x2(h_conv)
         
-  # Fully connected layer 1 -- after 2 round of downsampling, our 28x28 image
-  # is down to 7x7x64 feature maps -- maps this to 1024 features.
     with tf.name_scope('fc1'):
         W_fc1 = weight_variable([4 * 4 * 64, 1024])
         b_fc1 = bias_variable([1024])
@@ -59,13 +55,10 @@ def deepnn(x):
         h_pool2_flat = tf.reshape(h_pool, [-1, 4*4*64])        
         h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)  
 
-  # Dropout - controls the complexity of the model, prevents co-adaptation of
-  # features.
     with tf.name_scope('dropout'):
         keep_prob = tf.placeholder(tf.float32)
         h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-  # Map the 1024 features to 10 classes, one for each digit
     with tf.name_scope('fc2'):
         W_fc2 = weight_variable([1024, 10])
         b_fc2 = bias_variable([10])
@@ -75,39 +68,31 @@ def deepnn(x):
 
 
 def conv2d(x, W):
-    """conv2d returns a 2d convolution layer with full stride."""
     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 
 def max_pool_2x2(x):
-    """max_pool_2x2 downsamples a feature map by 2X."""
     return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                         strides=[1, 2, 2, 1], padding='SAME')
 
 
 def weight_variable(shape):
-    """weight_variable generates a weight variable of a given shape."""
     initial = tf.truncated_normal(shape, stddev=0.1)                  
     return tf.Variable(initial)                                      
                                                                      
 
 def bias_variable(shape):
-    """bias_variable generates a bias variable of a given shape."""
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
 
 
 def main(_):
-    # Import data
     print (FLAGS.data_dir)
     mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)   
 
-    # Create the model
     x = tf.placeholder(tf.float32, [None, 784])                    
-    # Define loss and optimizer
     y_ = tf.placeholder(tf.float32, [None, 10])
 
-    # Build the graph for the deep net
     with tf.device('cpu:0'):
         y_conv, keep_prob = deepnn(x)            
                                                                     
@@ -164,15 +149,11 @@ def main(_):
         print('test accuracy %g' % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
         
 def test(_):
-    # Import data
     mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)   
 
-    # Create the model
     x = tf.placeholder(tf.float32, [None, 784])                    
-    # Define loss and optimizer
     y_ = tf.placeholder(tf.float32, [None, 10])
 
-    # Build the graph for the deep net
     with tf.device('cpu:0'):
         y_conv, keep_prob = deepnn(x)            
                                                                     
