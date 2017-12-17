ec2978302d0c61d1e1bcd4b084d2f9c545000d88
seu5022 <kdhht5022@gmail.com>
2017-12-17 13:45:42 +0900

    remove repeated files

--- torch/lua programming/1. Getting Started/Exercise-1.8.lua
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env lua
--- Programming in Lua (Fourth edition)
--- file: Exercise-1.8.lua
-
--- $ lua Exercise-1.8.lua (arg[0]) DaeHaKim (arg[1])
-print("Hello!, My name is " .. arg[1])  -- Hello!, My name is DaehaKim
