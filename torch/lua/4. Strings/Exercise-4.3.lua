27cf0de3ef963f1d44b2a68f948dbbe5ba40bab7
seu5022 <kdhht5022@gmail.com>
2017-12-18 18:54:31 +0900

    add lua/4. Strings

---
+++ torch/lua/4. Strings/Exercise-4.3.lua
@@ -0,0 +1,34 @@
+#!/usr/bin/env lua
+-- Programming in Lua (Fourth edition)
+-- file: Exercise-4.3.lua
+
+function string.Split(str, delimiter)
+  return string.Explode(delimiter, str)
+end
+
+
+
+function insert_string1(strings, position, str)
+  part1= string.sub(strings, position)
+  final = str .. part1
+  return final
+end
+
+strings = "hello world"
+position = 1
+str = "start: "
+
+print(insert_string1(strings, position, str))
+
+function insert_string2(strings, position, str)
+  part1 = string.gmatch
+  part2 = string.sub(strings, position)
+  final = part1 .. str .. part2
+  return final
+end
+
+strings = "hello world"
+position = 7
+str = "small "
+
+print(insert_string2(strings, position, str))
