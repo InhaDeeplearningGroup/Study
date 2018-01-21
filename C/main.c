91c266bb83866849852878448250af7caea40061
seu5022 <kdhht5022@gmail.com>
2018-01-21 23:55:56 +0900

    add C study folder

---
+++ C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Chapter02-2
+//
+//  Created by Dae Ha Kim on 2018. 1. 21..
+//  Copyright © 2018년 Dae Ha Kim. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello Linux\n");
+    return 0;
+}
